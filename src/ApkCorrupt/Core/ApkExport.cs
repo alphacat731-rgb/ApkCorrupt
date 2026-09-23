9021678b81675e427418519f6f82f3c816f35caf
alphacat731-rgb <alphacat731@gmail.com>
2026-09-23 21:01:52 +0200

    Use MediaStore Files for reliable APK export

--- src/ApkCorrupt/Core/ApkExport.cs
+++ src/ApkCorrupt/Core/ApkExport.cs
@@ -1,5 +1,6 @@
 using Android.Content;
 using Android.Provider;
+using Android.OS;
 
 namespace ApkCorrupt.Core;
 
@@ -18,13 +19,13 @@ public static class ApkExport
             ?? throw new InvalidOperationException("No ContentResolver available.");
 
         var values = new ContentValues();
-        values.Put("display_name", displayName);
-        values.Put("mime_type", "application/vnd.android.package-archive");
-        values.Put("relative_path", "Download/");
+        values.Put(MediaStore.MediaColumns.DisplayName, displayName);
+        values.Put(MediaStore.MediaColumns.MimeType, "application/vnd.android.package-archive");
+        values.Put(MediaStore.MediaColumns.RelativePath, Environment.DirectoryDownloads + "/");
+        values.Put(MediaStore.MediaColumns.IsPending, 1);
 
-        var uri = resolver.Insert(
-            MediaStore.Downloads.ExternalContentUri,
-            values);
+        var collection = MediaStore.Files.GetContentUri(MediaStore.VolumeExternalPrimary);
+        var uri = resolver.Insert(collection, values);
 
         if (uri is null)
             throw new InvalidOperationException("Could not create the Downloads entry.");
@@ -36,6 +37,11 @@ public static class ApkExport
                 ?? throw new InvalidOperationException("Could not open the Downloads output.");
 
             await input.CopyToAsync(output, cancellationToken);
+
+            var published = new ContentValues();
+            published.Put(MediaStore.MediaColumns.IsPending, 0);
+            resolver.Update(uri, published, null, null);
+
             return uri.ToString()!;
         }
         catch
